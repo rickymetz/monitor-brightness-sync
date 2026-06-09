267ee8c22efb6dffe0e4813fd300254740cf3672
Sean Perkins <sean@mobility-labs.com>
2026-06-08 23:07:19 -0400

    feat: iOS app scaffold (xcodegen project, compiles for simulator)

---
+++ ios/Sources/MBSyncApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct MBSyncApp: App {
+  var body: some Scene {
+    WindowGroup {
+      Text("Monitor Brightness Sync")
+        .font(.title2)
+        .padding()
+    }
+  }
+}
